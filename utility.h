2865b3cd8596b983d48843f2f90bb8804b3960f1
Alexander Barunin <aabzel@yandex.ru>
2019-06-19 14:36:44 +0300

    Update utility.h

--- utility.h
+++ utility.h
@@ -1,1 +1,21 @@
 
+/** 
+* @file   
+* @brief 
+* 
+* @author Aleksandr Barunin
+* 
+* @date  Feb 2019
+*/
+
+#ifndef __UTILITY_H
+#define __UTILITY_H
+
+#include <stdint.h>
+
+#define   MAX(A,B)     ((A) <= (B) ? (B) : (A))
+#define   MIN(a,b)     ((a) <= (b) ? (a) : (b))
+
+
+
+#endif /* __UTILITY_H */
