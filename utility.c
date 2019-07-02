e14906fdb28e1eeed0c144391d09f7529d0c4220
Alexander Barunin <aabzel@yandex.ru>
2019-07-02 14:49:36 +0300

    Update utility.c

--- utility.c
+++ utility.c
@@ -1,5 +1,104 @@
 // C programming language handy functions
 
+time_t convertDateToUnixTime (RTC_TimeTypeDef const inTime,
+                              RTC_DateTypeDef const inDate) {
+    uint32_t y, m, d, t;
+
+    // Year
+    y = inDate.Year;
+    // Month of year
+    m = inDate.Month;
+    // Day of month
+    d = inDate.Date;
+
+    // January and February are counted as months 13 and 14 of the previous year
+    if (m <= 2) {
+        m += 12;
+        y -= 1;
+    }
+
+    // Convert years to days
+    t = (365 * y) + (y / 4) - (y / 100) + (y / 400);
+    // Convert months to days
+    t += (30 * m) + (3 * (m + 1) / 5) + d;
+    // Unix time starts on January 1st, 1970
+    t -= 719561;
+    // Convert days to seconds
+    t *= 86400;
+    // Add hours, minutes and seconds
+    t += (3600 * inTime.Hours) + (60 * inTime.Minutes) + inTime.Seconds;
+
+    // Return Unix time
+    return t;
+}
+
+
+void convertUnixTimeToDate (uint32_t t, RTC_TimeTypeDef *outTime,
+                            RTC_DateTypeDef *outDate) {
+    uint32_t a, b, c, d, e, f;
+    // Negative Unix time values are not supported
+    if (t < 1) {
+        t = 0;
+    }
+    // Retrieve hours, minutes and seconds
+    outTime->Seconds = t % 60;
+    t /= 60;
+    outTime->Minutes = t % 60;
+    t /= 60;
+    outTime->Hours = t % 24;
+    t /= 24;
+    // Convert Unix time to date
+    a = (uint32_t) ((4 * t + 102032) / 146097 + 15);
+    b = (uint32_t) (t + 2442113 + a - (a / 4));
+    c = (20 * b - 2442) / 7305;
+    d = b - 365 * c - (c / 4);
+    e = d * 1000 / 30601;
+    f = d - e * 30 - e * 601 / 1000;
+    // January and February are counted as months 13 and 14 of the previous year
+    if (e <= 13) {
+        c -= 4716;
+        e -= 1;
+    } else {
+        c -= 4715;
+        e -= 13;
+    }
+    // Retrieve year, month and day
+    outDate->Year = c;
+    outDate->Month = e;
+    outDate->Date = f;
+    // Calculate day of week
+    outDate->WeekDay = computeDayOfWeek (c, e, f);
+}
+
+/**
+ * @brief Calculate day of week
+ * @param[in] y Year
+ * @param[in] m Month of year (in range 1 to 12)
+ * @param[in] d Day of month (in range 1 to 31)
+ * @return Day of week (in range 1 to 7)
+ **/
+
+static uint8_t computeDayOfWeek (uint16_t y, uint8_t m, uint8_t d) {
+    uint32_t h, j, k;
+
+    // January and February are counted as months 13 and 14 of the previous year
+    if (m <= 2) {
+        m += 12;
+        y -= 1;
+    }
+
+    // J is the century
+    j = y / 100;
+    // K the year of the century
+    k = y % 100;
+
+    // Compute H using Zeller's congruence
+    h = d + (26 * (m + 1) / 10) + k + (k / 4) + (5 * j) + (j / 4);
+
+    // Return the day of the week
+    return ((h + 5) % 7) + 1;
+}
+
 
 void explore_byte_order (void)
 {
