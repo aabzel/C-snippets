2b884ce59bfc4c8d1c03bbb6236852d908179b51
Alexander Barunin <aabzel@yandex.ru>
2020-06-22 02:54:11 +0300

    Add files via upload

---
+++ bit_utils.h
@@ -0,0 +1,21 @@
+#ifndef UTILITY_H_
+#define UTILITY_H_
+
+#include "math.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool is_power_of_two (uint32_t const val);
+bool is_parity_odd (uint32_t const inVal);
+uint8_t count_set_bits (uint32_t const inVal32bit);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* UTILITY_H_ */
